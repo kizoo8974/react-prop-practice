8d104b7a214e01fa095e7f4e39caabeb446f1c3e
Joon Kim <kizoo8974@gamil.com>
2023-08-17 12:15:44 +0900

    react props practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header (props) {
+  console.log('props', props, props.title);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1><a href="/">{props.title}</a></h1>
+    </header>
+  )
+}
+
+function Nav (props) {
+  const lis = [
+        
+  ]
+
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Article (props) {
+  return (
+    <article>
+    <h2>{props.title}</h2>
+    {props.body}
+    </article>
+  )
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'react', body:'react is ...'}
+  ]
+
+  return (
+    <div>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hellom WEB"></Article>
+      <Article title="props" body="practice"></Article>
     </div>
+    
   );
 }
 
